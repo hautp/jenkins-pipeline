98adc99c10eb8c19f8ab8b4a781678faa5f371bd
Hau Tran <hau.tp2@homecredit.vn>
2020-02-12 23:46:41 +0700

    Add funcs file

---
+++ vars/build.groovy
@@ -0,0 +1,20 @@
+//void dockerBuildImage(String app_name, String tag, String args) {
+//	sh("docker build --build-arg -t ${app_name}:${tag} .")
+//}
+//
+//void dockerPushImage(String registry, String app_name, String tag) {
+//	sh("docker push ${registry}:${tag}")
+//}
+//
+//private void dockerPushImage(String registryAddress, String imageName, def tag, def username, def password) {
+//
+//
+//}
+//
+//
+//def String getDatetime(format="yyyyMMddHHmmss") {
+//	def now = new Date();
+//	return now.format(format, TimeZone.getTimeZone('UTC'));
+//}
+
+
---
+++ vars/funcs.groovy
@@ -0,0 +1,11 @@
+String getImageNameWithTag(String REGISTRY, String APP_NAME) {
+  return "${REGISTRY}/${APP_NAME}:${getDateCommit()}"
+}
+
+String getDateCommit() {
+  return getReturnedValue("git log -n 1 --pretty=format:%cd --date==format:%Y%m%d%H%M")
+}
+
+String getReturnedValue(String timestamp) {
+  sh([script: timestamp, returnStdout: true]).trim()
+}
